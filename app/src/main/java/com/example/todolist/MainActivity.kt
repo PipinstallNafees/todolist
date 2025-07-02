da2bd646987003b7780b48bf479b985fe61bea94
PipinstallNafees <nafeesurrahmankhan478@gmail.com>
2025-07-02 08:56:19 +0530

    name change

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TodolistTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Nafees",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
